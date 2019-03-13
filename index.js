5b40f1544aa5d9c268a0b57788486b916aabb68f
Ethan Freire <element55star@gmail.com>
2019-03-13 09:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
-
+var test = new Array()
+for(i = 0 ; i < musicians.length; i++){
+  string = "musicians[i] + " plays " + instruments[i] /n "
+statement =  test.push(string)
+}return statement
 }
